c68b59b783b9047f1d5393a44103c0b5dcdfb023
zoupan <895796932@qq.com>
2020-11-29 23:44:33 +0800

    stage1

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: web_demo
+ * @description:
+ * @author: zoupan
+ * @version: 1.0
+ * @create: 2020-11-29 23:40
+ **/
+
+@RestController
+public class WebController {
+
+    @GetMapping("/web")
+    public String web(){
+        return "web_demo";
+    }
+
+}
